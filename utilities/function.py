e55b54feb81e5484ca06d87ae6b9a6e8c859211c
Ai Sukmoren <Icecicle1@gmail.com>
2024-07-01 10:58:55 +0700

    ADD

--- utilities/function.py
+++ utilities/function.py
@@ -6,6 +6,7 @@ from langchain.graphs import Neo4jGraph
 from langchain.chains import GraphCypherQAChain
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
+from langchain_community.tools import YouTubeSearchTool
 # Load environment variables from .env file
 import os 
 load_dotenv()
@@ -28,3 +29,40 @@ class chain_function():
 
         return answer
     
+    from langchain_community.tools import YouTubeSearchTool
+    @staticmethod
+    def get_yotube_link(ques):
+        youtube = YouTubeSearchTool()
+        result = youtube.run(ques)
+        # Convert the string representation of the list back into a Python list
+        result_list = ast.literal_eval(result)
+
+        # Extract the first link
+        first_link = result_list[0]
+        
+        # Extract video ID from the URL
+        video_id = re.search(r'(?<=v=)[^&#]+', first_link).group()
+
+        # Construct the thumbnail URL
+        thumbnail_url = f'https://img.youtube.com/vi/{video_id}/0.jpg'
+
+        # Display the thumbnail using Streamlit
+        st.image(thumbnail_url)
+
+        # Use a markdown to display a clickable link with custom text
+        display_text = "link"  # Custom text for the link
+
+        template = PromptTemplate.from_template(
+            """
+            Yor task is to create a shot brief about a video user is looking for in excitment tone
+            Instruction:
+            - limit the word lenght to 50 words
+        
+            user prompt to find video on youtube: {ques}
+        
+            """
+        )
+        response = f"""
+        here your url [{display_text}]({first_link}) !
+        """
+        return response
